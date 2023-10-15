0bad68846e3b28d66e6a6e79db62306be0a72ac1
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-15 15:06:14 -0700

    feat: added environemnt variables

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { Knex, knex as setupKnex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found.')
+}
+
 export const databaseConfig: Knex.Config = {
-  client: 'sqlite',
+  client: process.env.DATABASE_CLIENT,
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
