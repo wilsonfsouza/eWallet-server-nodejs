a2e0f113c7993f49319f43b7446e1286db8d6ccb
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-15 15:05:56 -0700

    feat(server): tested adding a transaction into the database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test Transaction',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
