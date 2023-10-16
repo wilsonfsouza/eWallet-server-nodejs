35eb95950b84a74f4ceca36d933104ed89d8609a
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-15 19:19:50 -0700

    refactor(routes/server): created plugin for transaction routes and added test route to /hello endpoint

---
+++ src/routes/transactions.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const transaction = await knex('transactions')
+      .insert({
+        id: crypto.randomUUID(),
+        title: 'Test Transaction',
+        amount: 1000,
+      })
+      .returning('*')
+
+    return transaction
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Test Transaction',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
