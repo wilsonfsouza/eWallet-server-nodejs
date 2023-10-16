5d2a10eb9c441e80998a245898f28346b61f8505
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-15 19:43:36 -0700

    fix(env): converted port string to number. Corrected database client. Corrected passing process.env to safeParse

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,30 +1,19 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-const KNEX_CLIENTS = [
-  'pg',
-  'pg-native',
-  'sqlite3',
-  'better-sqlite3',
-  'mysql',
-  'mysql2',
-  'oracledb',
-  'tedious',
-] as const
-
 const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
-  DATABASE_CLIENT: z.enum(KNEX_CLIENTS).default('sqlite3'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
-const _env = envSchema.safeParse(envSchema)
+const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('Invalid environment variables!', _env.error.format)
+  console.error('Invalid environment variables!', _env.error.format())
 
   throw new Error('Invalid environment variables')
 }
