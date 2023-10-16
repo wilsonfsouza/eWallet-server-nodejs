b20a2fb80ef78897ab963d93698d0b9c006cc661
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-16 07:11:47 -0700

    feat(routes/transactions): added route to list all transactions and only 1 single transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,31 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const createTransactionParmsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = createTransactionParmsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select()
+      .where('id', id)
+      .first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
