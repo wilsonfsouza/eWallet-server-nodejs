19fd93eebc3fabfb77adf5fc999cecfebf955922
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-16 19:39:34 -0700

    feat(routes/transactions): created middleware to check for session id in the cookies

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
+
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
