ea9130795fc834ba24780c2ba5b63eab741c23bc
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-22 20:32:12 -0700

    feat(production): installed postgres client for production and moved sqlite to development only. Added different database connection config based on the client. Set minimum node version for project to run

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,21 @@
 import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
+const getConnectionConfigByDatabaseClient = (client: 'sqlite' | 'pg') => {
+  switch (client) {
+    case 'sqlite':
+      return {
+        filename: env.DATABASE_URL,
+      }
+    case 'pg':
+    default:
+      return env.DATABASE_URL
+  }
+}
+
 export const databaseConfig: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  connection: getConnectionConfigByDatabaseClient(env.DATABASE_CLIENT),
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
